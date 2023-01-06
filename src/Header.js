9fea195200e39df976bb5e91f2656a7f7e8e44ea
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-01-06 16:05:29 -0600

    scaffolding for react-app

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Header.css';
+
+class Header extends React.Component{
+  render() {
+    return(
+      <div>
+        <h1>Gallery of Horns</h1>
+        <h2>Let's go!!!</h2>
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <h3>Learning React Stuff</h3>
+    );
+  }
+}
+
+
+export default Main;
